30be4cbd4f11bdf6fb372f391b8ff6685560c802
ghanshyamverma4654 <ghanshyam.verma_cs19@gla.ac.in>
2022-02-13 03:17:20 +0530

    new class javaOutputFormating updated

---
+++ src/Hacker_Rank_Solutions/javaOutputFormatting.java
@@ -0,0 +1,28 @@
+package Hacker_Rank_Solutions;
+
+import java.util.Scanner;
+
+/*
+ * Author Name : Ghanshyam Verma
+ * Date : 13-02-2022
+ * JDK version : 17 version 17
+ * IDE : IntelliJ IDEA Ultimate
+ */
+public class javaOutputFormatting {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("================================");
+
+        for (int i = 0; i < 3; i++) {
+            String str = scan.next();
+            int num = scan.nextInt();
+            System.out.printf("%-15s%03d%n", str, num);
+        }
+
+        System.out.print("================================");
+
+    }
+}
